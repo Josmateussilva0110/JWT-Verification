e0fb70d245a3102964e8d6a1be4e2f1ca424a662
Jose Mateus <jmateussilva044@gmail.com>
2025-08-01 23:57:55 -0300

    feat: form pet completed

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -9,25 +9,47 @@ function PetForm({handleSubmit, petData, flag}) { // flag para editar ou cadastr
     const [preview, setPreview] = useState([])
     const colors = ['Branco', 'Preto', 'Caramelo', 'Mesclado']
 
-    function handleChange(e) {
-
+    function handleChange(event) {
+        setPet({...pet, [event.target.name]: event.target.value})
     }
 
-    function onFileChange(e) {
+    function onFileChange(event) {
+        const filesArray = Array.from(event.target.files)
+        const previews = filesArray.map(file => URL.createObjectURL(file))
 
+        setPet({ ...pet, photos: filesArray }) // Agora photos é um array
+        setPreview(previews)
     }
 
-    function handleColor(e) {
 
+    function handleColor(event) {
+        setPet({...pet, color: event.target.options[event.target.selectedIndex].text})
+    }
+
+    function submit(event) {
+        event.preventDefault()
+        handleSubmit(pet)
     }
 
     return (
-        <form className={formStyles.form_container}>
+        <form onSubmit={submit}  className={formStyles.form_container}>
             <Input text="Nome do Pet" type="text" name="name" placeholder="Digite o nome" handleOnChange={handleChange} value={pet.name || ''}/>
             <Input text="Idade do Pet" type="text" name="age" placeholder="Digite a idade" handleOnChange={handleChange} value={pet.age || ''}/>
             <Input text="Peso do Pet" type="number" name="weight" placeholder="Digite o peso" handleOnChange={handleChange} value={pet.weight || ''}/>
-            <Input text="Fotos do Pet" type="file" name="photos" handleOnChange={onFileChange} multiple={true}/>
             <Select name="color" text="Selecione uma cor" options={colors} handleOnChange={handleColor} value={pet.color || ''}></Select>
+            <Input text="Fotos do Pet" type="file" name="photos" handleOnChange={onFileChange} multiple={true}/>
+            <div className={formStyles.preview_pet_photos}>
+                {preview.length > 0
+                    ? preview.map((src, index) => (
+                        <img key={index} src={src} alt={`preview ${index}`} />
+                    ))
+                    : Array.isArray(pet.photos) &&
+                    pet.photos.map((photo, index) => (
+                        <img key={index} src={URL.createObjectURL(photo)} alt={`preview ${index}`} />
+                    ))
+                }
+            </div>
+
             <input type="submit" value={flag}></input>
         </form>
     )
--- frontend/src/components/pages/pet_pages/Register.js
+++ frontend/src/components/pages/pet_pages/Register.js
@@ -1,16 +1,59 @@
 import styles from './Register.module.css'
 import PetForm from '../../form/PetForm'
-
+import useFlashMessage from '../../../hooks/useFlashMessage'
+import api from '../../../utils/api'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 function RegisterPet() {
+    const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage } = useFlashMessage()
+    const navigate = useNavigate()
+
+    async function addPet(pet) {
+        const formData = new FormData()
+
+        Object.keys(pet).forEach((key) => {
+            if (key === 'photos') {
+                for (let i = 0; i < pet[key].length; i++) {
+                    formData.append('photos', pet[key][i])
+                }
+            } else {
+                formData.append(key, pet[key])
+            }
+        })
+
+        let msgType = 'success'
+        let data
+
+        //console.log(formData)
+
+        try {
+            const response = await api.post('/pet/register', formData, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(token)}`,
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+            data = response.data
+        } catch (err) {
+            msgType = 'error'
+            data = err.response?.data || { message: 'Erro ao cadastrar pet.' }
+        }
+
+        setFlashMessage(data.message, msgType)
+
+        if (msgType !== 'error') {
+            navigate('/pet/myPets')
+        }
+    }
+
     return (
         <section className={styles.addpet_header}>
             <div>
-                <h1>RegisterPet</h1>
+                <h1>Cadastrar Pet</h1>
             </div>
-            <PetForm flag="Cadastrar">
-                
-            </PetForm>
+            <PetForm handleSubmit={addPet} flag="Cadastrar" />
         </section>
     )
 }
