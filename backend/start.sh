e97dc9ed6bfc10bdab4ddd58abd439620311e70a
Jose Mateus <jmateussilva044@gmail.com>
2025-08-19 15:24:46 -0300

    feat: docker with hot-reload

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Espera o PostgreSQL estar pronto
+./wait-for-it.sh db
+
+# Roda as migrations mais recentes
+npx knex migrate:latest
+
+# Inicia o servidor com nodemon
+nodemon ./index.js
